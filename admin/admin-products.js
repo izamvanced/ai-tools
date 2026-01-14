6d28d30815bfbbef030e581fcacec1907199d255
izamvanced <izamvanced@gmail.com>
2026-01-15 06:40:15 +0700

    Update admin-products.js

--- admin/admin-products.js
+++ admin/admin-products.js
@@ -1,36 +1,12 @@
-import {
-  db,
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  doc,
-  serverTimestamp
-} from "./firebase.js";
-
 console.log("ADMIN PRODUCTS JS LOADED");
 
 document.addEventListener("DOMContentLoaded", () => {
-  alert("ADMIN PRODUCTS READY");
+  alert("DOM READY");
 
-  const nameInput = document.getElementById("name");
-  const slugInput = document.getElementById("slug");
-  const saveBtn = document.getElementById("saveProduct");
+  const btn = document.getElementById("saveProduct");
+  console.log("BUTTON:", btn);
 
-  saveBtn.addEventListener("click", async () => {
-    if (!nameInput.value || !slugInput.value) {
-      alert("Nama & slug wajib");
-      return;
-    }
-
-    await addDoc(collection(db, "products"), {
-      name: nameInput.value,
-      slug: slugInput.value,
-      status: "active",
-      createdAt: serverTimestamp(),
-      updatedAt: serverTimestamp()
-    });
-
-    alert("PRODUCT SAVED");
+  btn.addEventListener("click", () => {
+    alert("SAVE BUTTON CLICKED");
   });
 });

33bdd17dc6c93efa41346867e3ebbb125b15e1a8
izamvanced <izamvanced@gmail.com>
2026-01-15 04:39:40 +0700

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,10 +1,22 @@
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  doc,
+  serverTimestamp
+} from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIts39eYziTvJUVAvun91dIZRNjRO8Qes",
+  apiKey: "API_KEY_KAMU",
   authDomain: "ai-tools-library-ac10c.firebaseapp.com",
   projectId: "ai-tools-library-ac10c",
 };
@@ -13,3 +25,13 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// EXPORT ULANG FIRESTORE API
+export {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  doc,
+  serverTimestamp
+};

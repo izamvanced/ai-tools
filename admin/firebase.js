2f0f0b7f569ca732abf71f3629613061a593f8de
izamvanced <izamvanced@gmail.com>
2026-01-15 08:32:41 +0700

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,24 +1,14 @@
-
 import { initializeApp } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
 import { getAuth } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  deleteDoc,
-  doc,
-  serverTimestamp
+  getFirestore, collection, addDoc, serverTimestamp
 } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIts39eYziTvJUVAvun91dIZRNjRO8Qes",
+  apiKey: "API_KEY_KAMU",
   authDomain: "ai-tools-library-ac10c.firebaseapp.com",
   projectId: "ai-tools-library-ac10c"
 };
@@ -27,13 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-export {
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  deleteDoc,
-  doc,
-  serverTimestamp
-};
+export { collection, addDoc, serverTimestamp };

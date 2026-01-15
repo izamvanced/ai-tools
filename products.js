a50fbd0e2931cf658118690b783942a6c411ccac
izamvanced <izamvanced@gmail.com>
2026-01-15 20:46:43 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,10 +1,35 @@
 import { db } from "./firebase-public.js";
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-alert("PRODUCTS.JS + FIREBASE IMPORT OK");
+console.log("PRODUCTS JS LOADED");
 
-document.addEventListener("DOMContentLoaded", () => {
-  alert("DOM READY WITH FIREBASE");
+document.addEventListener("DOMContentLoaded", async () => {
+  console.log("PRODUCTS DOM READY");
 
   const grid = document.getElementById("productGrid");
-  grid.innerHTML = "<p>Firebase import sukses ✅</p>";
+  grid.innerHTML = "<p>Querying products...</p>";
+
+  const snapshot = await getDocs(collection(db, "products"));
+
+  console.log("PRODUCT COUNT:", snapshot.size);
+
+  if (snapshot.empty) {
+    grid.innerHTML = "<p>Products collection kosong.</p>";
+    return;
+  }
+
+  grid.innerHTML = "";
+
+  snapshot.forEach(doc => {
+    const data = doc.data();
+    grid.innerHTML += `
+      <div class="card">
+        <div class="product-name">${data.name}</div>
+        <div class="desc">${data.summary || ""}</div>
+      </div>
+    `;
+  });
 });

16a681db0b6faae6aab368381671bdcbcfcf4096
izamvanced <izamvanced@gmail.com>
2026-01-16 06:18:15 +0700

    Update products-archive.js

--- products-archive.js
+++ products-archive.js
@@ -1,1 +1,48 @@
+import { db } from "./firebase-public.js";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+document.addEventListener("DOMContentLoaded", async () => {
+  const grid = document.getElementById("productGrid");
+  if (!grid) return;
+
+  try {
+    const q = query(
+      collection(db, "products"),
+      where("status", "==", "active"),
+      orderBy("name")
+    );
+
+    const snapshot = await getDocs(q);
+
+    if (snapshot.empty) {
+      grid.innerHTML = "<p>Tidak ada produk.</p>";
+      return;
+    }
+
+    grid.innerHTML = "";
+
+    snapshot.forEach(doc => {
+      const p = doc.data();
+
+      grid.innerHTML += `
+        <div class="card">
+          <div class="product-name">${p.name}</div>
+          <div class="desc">${p.summary || ""}</div>
+          <a class="btn" href="./product.html?slug=${p.slug}">
+            Lihat Produk
+          </a>
+        </div>
+      `;
+    });
+
+  } catch (e) {
+    console.error(e);
+    grid.innerHTML = "<p>Gagal memuat produk.</p>";
+  }
+});
